37385ee544348f41a57388f0f78f03479e1c72b4
Squallog <lord.subbington@gmail.com>
2025-11-06 00:13:59 +0000

    pitchin in

--- timer.js
+++ timer.js
@@ -20,6 +20,6 @@ var seconds = Math.floor((deltaClock % (1000 * 60)) / 1000);
 if (nowMil < thenMil) {
     content.innerHTML = `<h2>${days}d / ${hours}h / ${minutes} m / ${seconds}s<h2>`;
 } else {
-    content.innerHTML = `<section> <p>Darryl Kenedy has a great story - it is relatable, motivational, and human - but the man has built a career around telling it. There is little scope for a story that is <em>new</em>. A peice covering his story will only ever be <em>fine</em>.</p> <p>Our <em>film-jam</em> submission was fine. But after resubmission was pushed up to 75%. Lets not waste our own potential again.</p><p>This is docs without rules.<br>There are no rules - there is no protection.</p><p><span>I am <em>taking control</em> of the project.</span></p></section><section><p>I'm going to make the film about <em>dogs</em>.</p></section>`
+    content.innerHTML = `<section> <p>Darryl Kenedy has a great story - it is relatable, motivational, and human - but the man has built a career around telling it. There is little scope for a story that is <em>new</em>. A peice covering his story will only ever be <em>fine</em>.</p> <p>Our <em>film-jam</em> submission was fine. But after resubmission was pushed up to 75%. Lets not waste our own potential again.</p><p>This is docs without rules.<br>There are no rules - there is no protection.</p><p><span>I am <em>taking control</em> of the project.</span></p></section><section><p>I'm going to make the film about <em>dogs</em>.</p><a href="pitch.html><span>CLICK ME FOR PITCH DETAILS<span></a></section>`
 }
 }, 1000);
